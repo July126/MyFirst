2998a284a6ac0a858140f00c538f35c1adb64946
MS <515523953@qq.com>
2015-12-04 09:51:33 +0800

    这是我的第一个GitHub仓库

---
+++ MyFirstGitHub/MyFirstGitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstGitHub
+//
+//  Created by MS on 15-12-4.
+//  Copyright (c) 2015年 周晓丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstGitHub/MyFirstGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstGitHub
+//
+//  Created by MS on 15-12-4.
+//  Copyright (c) 2015年 周晓丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
